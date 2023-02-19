8917d1e1995e5e6ba5eec8dc46c7a99c972d2130
ChueneVincent <chuene.yusuf@gmail.com>
2023-02-18 20:01:58 -0800

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,16 @@ for (n = 48; n <= 56; n++)
 for (m = 49; m <= 57; m++)
 {
 if (m > n)
-
-putchar(n);
-utchar(m);
+{
+   putchar(n);
+   putchar(m);
 if (n != 56 || m != 57)
 {
 putchar(',');
 putchar(' ');
-}
-}
-}
+   }
+  }
+ }
 }
 putchar('\n');
 return (0);
