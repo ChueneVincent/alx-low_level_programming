1564660baf47c95505b7fce0091bf16c25bae8d2
ChueneVincent <chuene.yusuf@gmail.com>
2023-03-02 16:06:37 -0800

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,34 +2,57 @@
 #include <stdio.h>
 
 /**
-* cap_string - a function that capitalizes all words of a string
+* cap_string - A function that capitalizes all words of a string.
 * @str: char type pointer
 *
 * Return: pointer
 */
 
-char *cap_string(char *s)
-{
-	int i = 0, j;
-	char a[] = " \t\n,;.!?\"(){}";
-	while (*(s + i))
-	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			if (i == 0)
-				*(s + i) -= 'a' - 'A';
-			else
-			{
-				for
-				       	(j = 0; j <= 12; j++)
+char *cap_string(char *str)
 
-				{
-					if (a[j] == *(s + i - 1))
-						*(s + i) -= 'a' - 'A';
-				}
-			}
+{
+
+	int i;
+
+	int j;
+
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
+
+	i = 0;
+
+	while (str[i] != '\0')
+
+	{
+
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+
+		{
+
+			str[i] = str[i] - 32;
+
 		}
+
+		j = 0;
+
+		while (c[j] != '\0')
+
+		{
+
+			if (c[j] == str[i] && (str[i + 1] >= 97 && str[i + 1] <= 122))
+
+			{
+
+				str[i + 1] = str[i + 1] - 32;
+
+			}
+
+			j++;
+
+		}
+
 		i++;
+
 	}
-	return (s);
+
+	return (str);
 }
