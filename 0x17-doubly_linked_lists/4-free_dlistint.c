059dbbe8ecda995dd15767dcc78efdd8a059afe0
ChueneVincent <chuene.yusuf.@gmail.com>
2023-08-18 05:41:12 -0700

    Task4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -7,13 +8,14 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+    dlistint_t *current = head;
     dlistint_t *next;
 
-    while (head != NULL)
+    while (current != NULL)
     {
-        next = head->next;
-        free(head);
-        head = next;
+        next = current->next;
+        free(current);
+        current = next;
     }
 }
 
