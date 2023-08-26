6864d0c76903cd1b24d075120f9bc9f99e39b3c5
ChueneVincent <chuene.yusuf@gmail.com>
2023-08-26 01:28:35 +0000

    me

---
+++ 0x18-dynamic_libraries/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Function prototypes */
+void print_hello(void);
+int add(int a, int b);
+
+/* Type definition */
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+#endif /* HOLBERTON_H */
+
